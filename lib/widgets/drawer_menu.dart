7dffd5baa17978623cad77ee3664f7585efc4553
‘terupro’ <terupro911@gmail.com>
2023-04-13 19:42:27 +0900

    update drawer_menu.dart

--- lib/widgets/drawer_menu.dart
+++ lib/widgets/drawer_menu.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:share_plus/share_plus.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+final Uri policyUrl = Uri.parse(
+    'https://celestial-clam-8a3.notion.site/a5c323e0053d4bda88a1584a2f941fce');
+
+final Uri ruleUrl = Uri.parse(
+    'https://celestial-clam-8a3.notion.site/d5d75deaea7d442293b4d3eb75868a02');
+
+final Uri helpUrl = Uri.parse(
+    'https://celestial-clam-8a3.notion.site/AIChats-4294b075a76341d8955ec3374aa5f8db');
 
 class DrawerMenu extends StatefulWidget {
   const DrawerMenu({super.key});
@@ -65,31 +75,23 @@ class _DrawerMenuState extends State<DrawerMenu> {
           ListTile(
             leading: const Icon(Icons.privacy_tip_outlined),
             title: const Text('プライバシーポリシー'),
-            onTap: () {
-              Navigator.pop(context);
-            },
+            onTap: _launchPolicyUrl,
           ),
           ListTile(
             leading: const Icon(Icons.description_outlined),
             title: const Text('利用規約'),
-            onTap: () {
-              Navigator.pop(context);
-            },
+            onTap: _launchRuleUrl,
           ),
           const Divider(),
           ListTile(
             leading: const Icon(Icons.star_outline),
             title: const Text('プレミアムプランに登録する'),
-            onTap: () {
-              Navigator.pop(context);
-            },
+            onTap: _launchHelpUrl,
           ),
           ListTile(
             leading: const Icon(Icons.help_outline),
             title: const Text('ヘルプ＆サポート'),
-            onTap: () {
-              Navigator.pop(context);
-            },
+            onTap: _launchHelpUrl,
           ),
         ],
       ),
@@ -98,6 +100,24 @@ class _DrawerMenuState extends State<DrawerMenu> {
 
   void _shareApp() async {
     await Share.share(
-        'AIChatsアプリで手軽にAIと会話しよう！言語やトピックを選んで、いつでもどこでも会話ができます。友達や家族と共有して、みんなでAIとの会話を楽しんでみませんか？シェアリンクはこちら：https://example.com/aichats」 https://example.com/aichats');
+        'AIChatsアプリで手軽にAIと会話しよう！最先端AIと、音声やテキストを使って会話できます。友達や家族と共有して、みんなでAIとの会話を楽しんでみませんか？シェアリンクはこちら：https://example.com/aichats」 https://example.com/aichats');
+  }
+
+  Future<void> _launchPolicyUrl() async {
+    if (!await launchUrl(policyUrl)) {
+      throw Exception('無効なURLです。');
+    }
+  }
+
+  Future<void> _launchRuleUrl() async {
+    if (!await launchUrl(ruleUrl)) {
+      throw Exception('無効なURLです。');
+    }
+  }
+
+  Future<void> _launchHelpUrl() async {
+    if (!await launchUrl(helpUrl)) {
+      throw Exception('無効なURLです。');
+    }
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,10 +9,12 @@ import path_provider_foundation
 import share_plus
 import shared_preferences_foundation
 import speech_to_text_macos
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
